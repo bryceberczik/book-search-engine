25f35421812ba6b581f6e353947231285834b203
bryceberczik <berczikbryce@gmail.com>
2024-11-11 18:05:05 -0600

    readjusted auth.ts file in backend

--- server/src/services/auth.ts
+++ server/src/services/auth.ts
@@ -1,38 +1,34 @@
-import type { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 interface JwtPayload {
-  _id: unknown;
+  _id: string;
   username: string;
-  email: string,
+  email: string;
 }
 
-export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
-  const authHeader = req.headers.authorization;
+export const authenticateToken = ({ req }: any) => {
+  
+  let token = req.body.token || req.query.token || req.headers.authorization;
 
-  if (authHeader) {
-    const token = authHeader.split(' ')[1];
+  if (req.headers.authorization) token = token.split(' ').pop().trim();
+  if (!token) return req;
 
-    const secretKey = process.env.JWT_SECRET_KEY || '';
-
-    jwt.verify(token, secretKey, (err, user) => {
-      if (err) {
-        return res.sendStatus(403); // Forbidden
-      }
-
-      req.user = user as JwtPayload;
-      return next();
-    });
-  } else {
-    res.sendStatus(401); // Unauthorized
+  try {
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY || '', { maxAge: '2hr' });
+    req.user = decodedToken;
+  } catch (err) {
+    console.log(err);
+    console.log('Invalid token');
   }
+
+  return req;
 };
 
-export const signToken = (username: string, email: string, _id: unknown) => {
-  const payload = { username, email, _id };
+export const signToken = (username: string, email: string, _id: string) => {
+  const payload: JwtPayload = { username, email, _id };
   const secretKey = process.env.JWT_SECRET_KEY || '';
 
   return jwt.sign(payload, secretKey, { expiresIn: '1h' });
