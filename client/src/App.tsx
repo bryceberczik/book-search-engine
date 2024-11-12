af9ba372909569e9c954696bd4ddf205534779b7
bryceberczik <berczikbryce@gmail.com>
2024-11-11 18:24:23 -0600

    added app.tsx graphQl setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,38 @@
-import './App.css';
-import { Outlet } from 'react-router-dom';
-
-import Navbar from './components/Navbar';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import AuthService from "./utils/auth";
 
 function App() {
+  const httpLink = createHttpLink({ uri: "/graphql" });
+
+  const authLink = setContext((_, { headers }) => {
+    const token = AuthService.getToken();
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    };
+  });
+
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <>
+    <ApolloProvider client={client}>
       <Navbar />
       <Outlet />
-    </>
+    </ApolloProvider>
   );
 }
 
