982abcf5c6d5ba1822d91f1314ac7451f40b4c39
bryceberczik <berczikbryce@gmail.com>
2024-11-11 18:22:40 -0600

    added mutations, queries ts files in the front end

---
+++ client/src/utils/mutations.ts
@@ -0,0 +1,74 @@
+import { gql } from '@apollo/client';
+
+export const CREATE_USER = gql`
+  mutation($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation AddUser($input: UserInput!) {
+    addUser(input: $input) {
+      token
+      user {
+        _id
+        email
+        username
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation SaveBook($input: BookInput!) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        title
+        authors
+        description
+        image
+      }
+    }
+  }
+`;
+
+export const DELETE_BOOK = gql`
+  mutation($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      savedBooks {
+        bookId
+        title
+        authors
+        description
+        image
+        link
+      }
+    }
+  }
+`;
